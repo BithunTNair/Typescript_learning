d97082941f47035ede48d5e03b4598daa59ea29f
BithunT-Nair <react666cr@gmail.com>
2025-01-26 23:53:06 +0530

    second commit

--- index.js
+++ index.js
@@ -7,3 +7,67 @@ var object = {
     name: "Bithun",
     age: 24,
 };
+;
+var object1 = {
+    name: "Bithun",
+    age: 24,
+    getName: function () {
+        console.log(this.name);
+    },
+};
+var object2 = {
+    name: "Bithun",
+    age: 24,
+    getName: function () {
+        console.log(this.name);
+    },
+};
+// union
+var object3 = {
+    name: "Bithun",
+    age: '5'
+};
+var array2 = [1, 2, 3, 'hai'];
+// functions
+function getUserName(object2) {
+    return object2.name;
+}
+;
+var value = getUserName(object2);
+function getUserName1(_a) {
+    var name = _a.name, age = _a.age;
+    return name;
+}
+;
+getUserName1({ name: 'Bithun', age: 24 });
+function getUserName2(object2) {
+    console.log(object2.name);
+}
+;
+var friend = 'Athira';
+function add(a, b) {
+    return a + b;
+}
+;
+add(2, 2); //4
+add('2', '2'); //22
+//Generics
+function getAge(age) {
+    return age;
+}
+;
+getAge(24);
+getAge('24');
+//Enum
+var StatusType;
+(function (StatusType) {
+    StatusType[StatusType["PENDING"] = 0] = "PENDING";
+    StatusType[StatusType["COMPLETED"] = 1] = "COMPLETED";
+    StatusType[StatusType["FAILED"] = 2] = "FAILED";
+})(StatusType || (StatusType = {}));
+;
+function getStatus(orderId, status) {
+    console.log(orderId, '=', status);
+}
+;
+getStatus('123', StatusType.COMPLETED);
--- index.ts
+++ index.ts
@@ -55,15 +55,70 @@ let array2: (string | number)[] = [1, 2, 3, 'hai'];
 
 // functions
 
-function getUserName(object2: Details1):string {
+function getUserName(object2: Details1): string {
     return object2.name
 };
-const value=getUserName(object2);
+const value = getUserName(object2);
 
 
 
 
-function getUserName1({name,age}:{name:string;age:number}){
-return name
+function getUserName1({ name, age }: { name: string; age: number }) {
+    return name
 };
-getUserName1({name:'Bithun',age:24})
+getUserName1({ name: 'Bithun', age: 24 })
+
+function getUserName2(object2: Details1): void {
+    console.log(object2.name);
+
+};
+
+// Named types
+type Names = 'Athira' | 'Anu'
+let friend: Names = 'Athira';
+
+// function overloading
+function add(a: number, b: number): number;
+function add(a: string, b: string): string
+function add(a: any, b: any): any {
+    return a + b
+};
+
+add(2, 2);  //4
+add('2', '2'); //22
+
+//Generics
+function getAge<T>(age: T): T {
+    return age
+};
+
+getAge<number>(24);
+getAge<string>('24');
+
+//Enum
+// enum StatusType{
+//     PENDING,
+//     COMPLETED,
+//     FAILED
+// };
+// function getStatus(orderId:string,status:StatusType){
+// console.log(orderId ,'=',status);
+
+// };
+// getStatus('123',StatusType.COMPLETED);
+
+//as const
+let fname = 'Athira' as const;
+
+//typeof keyof
+const StatusType = {
+    PENDING: 'pending',
+    COMPLETED: 'completed',
+    FAILED: 'failed'
+} as const;
+
+function getStatus(orderId: string, status: keyof typeof StatusType) {
+    console.log(orderId, StatusType[status]);
+
+};
+getStatus('123', 'COMPLETED')
